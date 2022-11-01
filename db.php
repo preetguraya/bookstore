3b184adec6c4ce6a80135aa68978a71fd4983763
Gurpreet Kaur <gkguraya5@gmail.com>
2022-10-31 20:20:41 -0700

    m

--- db.php
+++ db.php
@@ -2,5 +2,10 @@
                                                     
 $con=mysqli_connect('localhost','gkguraya5','Bs2503047575','bookstore');  
                                                    //DB_CONNECTION FILE//
-												   
+if(!$con)	{
+    die("Connection failed" . mysqli_connect_error())
+ }   else{
+echo "connected successfully";
+    }
+										   
 ?>   												   
